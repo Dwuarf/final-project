1bf265b0aa1eab0492d04f44f821fa34dc3d62bb
Dwuarf <43864209+Dwuarf@users.noreply.github.com>
2019-11-20 11:16:26 -0500

    Add files via upload

--- FormValidation.js
+++ FormValidation.js
@@ -3,13 +3,11 @@ function validateForm(){
 }
 
 function displayAltAddress(){
-    var box = document.getElementById("displayAlt");
-    var alt = document.getElementsByName("altAddress");
+    var box = document.getElementById("displayAlt");    //Variable for the checkbox element
+    var alt = document.getElementsByName("altAddress"); //Variable for the alternate address label and text box
 
-    
-
-    switch(box.checked){
-        case true:
+    switch(box.checked){        //Simple switch that uses the true/false checked property
+        case true:              //To display or hide the alternate address line
             for(i = 0; i <alt.length; i++){
                 alt[i].style = "visibility: visible;";
             }
