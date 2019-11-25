08b6e37d60957e5dc028159914fc584017592961
Josh <joshualaforest@delta.edu>
2019-11-25 12:35:29 -0500

    began work on javascript form validation

--- FormValidation.js
+++ FormValidation.js
@@ -1,3 +1,27 @@
+var email = document.getElementById("email");
+
+email.addEventListener('onFocus', setStyle(validEmail(email.value)));
+
+function validEmail(emailString){
+    if(emailString.contains("@")){
+        alert("there is an @");
+        return true;
+    }
+    else{
+        alert("there is no @");
+        return false;
+    }
+}
+
+function setStyle(valid){
+    if(valid){
+        email.style = "color: red; border: 2px solid red;";
+    }
+    else{
+        email.style = "";
+    }
+}
+
 function validateForm(){
     
 }
